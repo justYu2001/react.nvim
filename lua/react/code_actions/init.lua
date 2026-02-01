4f6e3cc69ae2d2a6b4e3e054a8ddd5c2f118fd59
justYu2001 <jack4781012@gmail.com>
2026-02-01 20:07:12 +0800

    feat: implement create ref code action

--- lua/react/code_actions/init.lua
+++ lua/react/code_actions/init.lua
@@ -26,6 +26,7 @@ function M.get_sources(null_ls)
         require("react.code_actions.wrap_use_callback").get_source(null_ls),
         require("react.code_actions.generate_event_handler").get_source(null_ls),
         require("react.code_actions.generate_handler").get_source(null_ls),
+        require("react.code_actions.create_ref").get_source(null_ls),
     }
 end
 
